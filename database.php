77deeeeb0f623e66a26f85fbc74fd7455726cef7
TNTTheLagger <tntcsatornaja@gmail.com>
2024-01-10 20:27:15 +0200

    try

--- database.php
+++ database.php
@@ -1,7 +1,12 @@
 <?php
       try {
+<<<<<<< HEAD
             $conn = mysqli_connect("tunetracer.hu", "tunetracer", "tunetercer123321", "konyvtarak");
+=======
+            $conn = mysqli_connect("tunetracer.hu", "tunetracer", "tunetracer123321", "tunetracer");
+>>>>>>> parent of ccde2ed (Merge branch 'main' of https://github.com/TuneTracer/TuneTracer)
       } catch (Exception $e) {
+            echo $e;
             die("Nem sikerült csatlakozni");
       }
 ?>
